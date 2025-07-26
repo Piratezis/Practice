0b49982adcb6471da68d7ef87fcb460235c03285
Trace Pulse <138300084+Piratezis@users.noreply.github.com>
2025-07-26 14:31:25 +0500

    Update app.js
    Работающая версия. Проверил через Node

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /* https://Piratezis.github.io/Practice - .html 
 -____________________________________________________ЗАМЕТКИ
+Неблочные 
 Блочные внутри {}
  let - изменяемые типы данных
  const - неизменяемые типы данных
@@ -17,7 +18,7 @@ const i of numbers_list == i for i in numbers_list  -- цикл for для ма�
 const list_len = 100;
 
 //Функции
-function generate_numbers(let list_numbers_additional, const len_list_additional){
+function generate_numbers(list_numbers_additional, len_list_additional){
   //Генерация списка из чисел
   for (let i = 0; i < len_list_additional; i++){
     let new_random_number_fractional = Math.random(); // генерит от 0.0 до 1.0
@@ -27,8 +28,10 @@ function generate_numbers(let list_numbers_additional, const len_list_additional
   return list_numbers_additional;
 }
 
-function calculate_numbers(let array, const list_numbers_len){
+function calculate_numbers(array, list_numbers_len){
+
   // Подсчет значений для списка здесь: среднее, уникальные значения и т.д.
+
   let unique_list_numbers = [...new Set(array)]; // уникальный список
   let sum = 0;
 
@@ -37,9 +40,9 @@ function calculate_numbers(let array, const list_numbers_len){
   }
 
   let average = sum / list_numbers_len; // среднее значение
-  let aboveAverageCount = calculate_lenArray(array.filter(num => num > average)); // количество элементов выше среднего
-  let belowAverageCount = calculate_lenArray(array.filter(num => num < average)); // элементов ниже среднего
-  let below20Count = calculate_lenArray(array.filter(num => num < 20));           // элементов ниже 20
+  let more_average_numbers = calculate_lenArray(array.filter(num => num > average)); // количество элементов выше среднего
+  let below_average_numbers = calculate_lenArray(array.filter(num => num < average)); // элементов ниже среднего
+  let below20_numbers = calculate_lenArray(array.filter(num => num < 20));           // элементов ниже 20
 
   return {
     unique_list_numbers,
@@ -50,7 +53,7 @@ function calculate_numbers(let array, const list_numbers_len){
   };
 }
 
-function calculate_lenArray(let array){
+function calculate_lenArray(array){
   //___________________________________ Подсчет длины списка
   return array.length; 
 }
@@ -61,9 +64,12 @@ let list_numbers_values = generate_numbers([], list_len);
 const list_numbers_len = calculate_lenArray(list_numbers_values);
 
 let {unique_list_numbers, average, more_average_numbers, below_average_numbers, below20_numbers} = calculate_numbers(list_numbers_values, list_numbers_len);
-// Вызов вывода
-output_result_on_display(list_numbers_len, average, more_average_numbers, below_average_numbers, below20_numbers, unique_list_numbers, list_numbers_values);
-
+console.log("Список\n")
+console.log(unique_list_numbers, "\n")
+console.log("Среднее значени: ", average)
+console.log("Кол-во больше среднего: ", more_average_numbers)
+console.log("Количество меньше среднего: ", below_average_numbers)
+console.log("Кол-во меньше 20: ", below20_numbers)
 
 
 
